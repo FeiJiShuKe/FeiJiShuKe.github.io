7d896fbaf7ebc550871220141180725dce51cad1
FeiJiShuKe <q749062242@sina.com>
2018-09-21 19:35:12 +0800

    work4

--- work/js/na.js
+++ work/js/na.js
@@ -23,31 +23,43 @@ if (browser.versions.mobile) {//判断是否是移动设备打开。browser代�
     if (ua.match(/MicroMessenger/i) == "micromessenger") {
         //在微信中打开
         oXTB.style.opacity = '0';
+        oXTB.style.display = 'none'
+        oWXB.style.display = 'block';
         oWXB.style.opacity = '1';
     }
     if (ua.match(/WeiBo/i) == "weibo") {
         //在新浪微博客户端打开
         oXTB.style.opacity = '0';
+        oXTB.style.display = 'none'
+        oWXB.style.display = 'block';
         oWXB.style.opacity = '1';
     }
     if (ua.match(/QQ/i) == "qq") {
         //在QQ空间打开
         oXTB.style.opacity = '0';
+        oXTB.style.display = 'none'
+        oWXB.style.display = 'block';
         oWXB.style.opacity = '1';
     }
     if (browser.versions.ios) {
         //是否在IOS浏览器打开
         oWXB.style.opacity = '0';
+        oWXB.style.display = 'none'
+        oXTB.style.display = 'block';
         oXTB.style.opacity = '1';
     }
     if(browser.versions.android){
         //是否在安卓浏览器打开
         oWXB.style.opacity = '0';
+        oWXB.style.display = 'none'
+        oXTB.style.display = 'block';
         oXTB.style.opacity = '1';
     }
     if(browser.versions.webApp){
         //是否在web应用程序
         oWXB.style.opacity = '0';
+        oWXB.style.display = 'none'
+        oXTB.style.display = 'block';
         oXTB.style.opacity = '1';
     }
 } else {
